627505d11735b5cb477b55001bfdc63335303c6b
itallo <itallomkspop@gmail.com>
2022-09-29 20:49:34 -0300

    implement auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container } from 'react-bootstrap'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { PageHeader } from './components/layouts/PageHeader'
-import { Home } from './components/pages/Home'
-import { NotFound } from './components/pages/statusCode/NotFound'
+import { PageLayout } from './components/layouts/page/PageLayout'
+import { AuthProvider } from './contexts/auth'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <PageHeader />
-      <Container as="main">
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
+    <AuthProvider>
+      <PageLayout />
+    </AuthProvider>
   )
 }
---
+++ src/components/layouts/page/PageLayout.tsx
@@ -0,0 +1,40 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useAuth } from "../../../hooks/useAuth";
+import { Login } from "../../pages/auth/Login";
+import { Register } from "../../pages/auth/Register";
+import { Home } from "../../pages/Home";
+import { NotFound } from "../../pages/statusCode/NotFound";
+import { PageHeader } from "./PageHeader";
+
+export function PageLayout() {
+  const { authenticated } = useAuth()
+  
+  const authRoutes = (
+    <>
+
+    </>
+  )
+
+  const unAuthRoutes = (
+    <>
+      <Route path="" element={<Home />} />
+
+      <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
+    </>
+  )
+
+  return (
+    <BrowserRouter>
+      <PageHeader />
+      <Container as="main">
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+
+          { authenticated ? authRoutes : unAuthRoutes }
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  )
+} 
